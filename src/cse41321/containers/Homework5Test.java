07650290aefb97ccc7d9fe57412b2f79ca160d7a
Edgar Cole <edgarcolejr@gmail.com>
2021-07-28 10:28:43 -0700

    Okay. It looks like the SinglyLinkedList constructor is working.
    Additionally, this test includes the insertHead method as well.

--- src/cse41321/containers/Homework5Test.java
+++ src/cse41321/containers/Homework5Test.java
@@ -1,6 +1,7 @@
 package cse41321.containers;
 
 import cse41321.containers.Homework5.KeyValuePair;
+import cse41321.containers.Homework5.SinglyLinkedList;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -26,4 +27,13 @@ public class Homework5Test {
         assertEquals(keyValuePair.getValue(), "Edgar Cole");
     }
 
+    @Test
+    public void testTheSinglyLinkedListConstructor() {
+        KeyValuePair<Integer, String> keyValuePair = new KeyValuePair<>(548962115, "Edgar Cole");
+        SinglyLinkedList<KeyValuePair<Integer, String>> singlyLinkedList = new SinglyLinkedList<>();
+        singlyLinkedList.insertHead(keyValuePair);
+        assertEquals(singlyLinkedList.getHead().getData().getValue(),"Edgar Cole");
+        // Here we go again with the explicit cast. I guess I still have a bit to learn about how generics work.
+        assertEquals((int) singlyLinkedList.getHead().getData().getKey(),548962115);
+    }
 }
